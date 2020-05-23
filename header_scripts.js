7358eeb43119bb2abe8681595e85565824e7eed5
robjberryhill <robjberryhill@gmail.com>
2020-05-22 21:43:55 -0700

    JS toggle menus and aria support

--- header_scripts.js
+++ header_scripts.js
@@ -1,72 +1,82 @@
-
 const createEvent = (evt, handler, element) => {
   if (document.addEventListener) {
     element.addEventListener(evt, handler);
   } else if (document.attachEvent) {
     element.attachEvent(evt, handler);
   }
-}
+};
 
 const mblMnuTgl = (evt) => {
-  let navCntElm = document.getElementById('NavCont');
-  let navElmIsAcv = navCntElm.classList.contains('mobile-active');
+  let navCntElm = document.getElementById("NavCont");
+  let navElmIsAcv = navCntElm.classList.contains("mobile-active");
   let elm = evt.currentTarget;
-  let emlIsAcv = elm.classList.contains('is-active');
+  let emlIsAcv = elm.classList.contains("is-active");
 
   if (emlIsAcv && navElmIsAcv) {
-    elm.classList.remove('is-active');
-    navCntElm.classList.remove('mobile-active');
+    elm.classList.remove("is-active");
+    navCntElm.classList.remove("mobile-active");
   } else if (!emlIsAcv && !navElmIsAcv) {
-    elm.classList.add('is-active');
-    navCntElm.classList.add('mobile-active');
+    elm.classList.add("is-active");
+    navCntElm.classList.add("mobile-active");
   }
-}
+};
 
 const mblDrpDwn = (evt) => {
-  let elm = evt.currentTarget, elmAtr, elmPrt;
-  //console.log(elm)
+  let elm = evt.currentTarget,
+    elmAtr,
+    elmPrt,
+    elmMnu,
+    elmPrtMnuId;
 
-  if (elm.classList.contains('drp-dwn-toggle')) {
+  console.log(elm)
+
+  if (elm.classList.contains("drp-dwn-toggle")) {
     elmPrt = elm.parentElement;
-  } else if (elm.classList.contains('drp-dwn-cls')) {
+  } else if (elm.classList.contains("drp-dwn-cls")) {
     elmPrt = elm.parentElement.parentElement;
   }
 
-  elmAtr = elmPrt.getAttribute('aria-expanded');
+  elmAtr = elmPrt.getAttribute("aria-expanded");
+  elmPrtMnuId = elmPrt.getAttribute('aria-controls')
+  elmMnu = document.getElementById(elmPrtMnuId);
 
-  if (elmAtr === 'false') {
-    elmPrt.setAttribute('aria-expanded', 'true');
+  // console.log(elmPrtMnuId)
+  // console.log(elmMnu)
+
+  if (elmAtr === "false") {
+    elmPrt.setAttribute("aria-expanded", "true");
+    elmMnu.setAttribute("aria-hidden", "false");
   } else if (elmAtr === "true") {
-    elmPrt.setAttribute('aria-expanded', 'false');
+    elmPrt.setAttribute("aria-expanded", "false");
+    elmMnu.setAttribute("aria-hidden", "true");
   }
-}
+};
 
 const drpDwnEvts = () => {
-  let mblDrpDwnElm = document.getElementsByClassName('drp-dwn-toggle');
-  let mblDrpDwnCls = document.getElementsByClassName('drp-dwn-cls');
+  let mblDrpDwnElm = document.getElementsByClassName("drp-dwn-toggle");
+  let mblDrpDwnCls = document.getElementsByClassName("drp-dwn-cls");
 
   for (let i = 0; i < mblDrpDwnElm.length; i++) {
     let elm = mblDrpDwnElm[i];
-    createEvent('click', mblDrpDwn, elm);
+    createEvent("click", mblDrpDwn, elm);
   }
 
   for (let i = 0; i < mblDrpDwnCls.length; i++) {
     let elm = mblDrpDwnCls[i];
-    createEvent('click', mblDrpDwn, elm);
+    createEvent("click", mblDrpDwn, elm);
   }
-}
+};
 
 const intMblTgl = () => {
-  const mblBtnTglElm = document.getElementById('MobileToggle');
-  createEvent('click', mblMnuTgl, mblBtnTglElm);
-}
+  const mblBtnTglElm = document.getElementById("MobileToggle");
+  createEvent("click", mblMnuTgl, mblBtnTglElm);
+};
 
 const init = () => {
   //console.log('Javascript initiated.');
   intMblTgl();
   drpDwnEvts();
-}
-
+};
 
 const DocumentReadyInit = (fnm, bo) => {
   "use strict";
@@ -85,22 +95,29 @@ const DocumentReadyInit = (fnm, bo) => {
       }
       arl = [];
     }
-  }
+  };
 
   const ReadyStateChange = () => {
     if (document.readyState === "complete") {
       Ready();
     }
-  }
+  };
 
   bo[fnm] = function (callback, context) {
-    if (typeof callback !== "function") { throw new TypeError("callback for docReady must be a function"); }
+    if (typeof callback !== "function") {
+      throw new TypeError("callback for docReady must be a function");
+    }
     if (arf) {
-      setTimeout(() => { callback(context); }, 1);
+      setTimeout(() => {
+        callback(context);
+      }, 1);
     } else {
       arl.push({ fn: callback, ctx: context });
     }
-    if (document.readyState === "complete" || (!document.attachEvent && document.readyState === "interactive")) {
+    if (
+      document.readyState === "complete" ||
+      (!document.attachEvent && document.readyState === "interactive")
+    ) {
       setTimeout(Ready, 1);
     } else if (!rehinstalled) {
       if (document.addEventListener) {
@@ -112,9 +129,8 @@ const DocumentReadyInit = (fnm, bo) => {
       }
       rehinstalled = true;
     }
-  }
-
-}
+  };
+};
 
 DocumentReadyInit("docReady", window);
 
